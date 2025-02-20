8c78f125fc58d62ad7d149adf1fed43ed54937e4
Shengyu HUANG <h_shengyu@yahoo.com>
2025-02-20 11:39:38 +0100

    Update download_data_weight.sh

--- scripts/download_data_weight.sh
+++ scripts/download_data_weight.sh
@@ -1,5 +1,5 @@
-wget --no-check-certificate --show-progress https://share.phys.ethz.ch/~gsg/Predator/data.zip
-wget --no-check-certificate --show-progress https://share.phys.ethz.ch/~gsg/Predator/weights.zip
+wget --no-check-certificate --show-progress https://share.phys.ethz.ch/~gseg/Predator/data.zip
+wget --no-check-certificate --show-progress https://share.phys.ethz.ch/~gseg/Predator/weights.zip
 unzip data.zip
 unzip weights.zip
 rm data.zip
